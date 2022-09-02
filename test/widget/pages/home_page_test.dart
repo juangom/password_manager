322f48b4fa31a8e4fc5cb1c1a8aa38731f04fd00
Juan <juancgomezcastro@gmail.com>
2022-09-01 21:54:41 -0600

    Refactor test dirs

---
+++ test/widget/pages/home_page_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:password_manager/application/ui/pages/home_page.dart';
+import 'package:password_manager/domain/repositories/password_repository.dart';
+import 'package:password_manager/domain/repositories/storage_repository.dart';
+import 'package:password_manager/main.dart';
+
+class MockPasswordListRepository extends Mock
+    implements PasswordListRepository {}
+
+class MockStorageRepository extends Mock implements StorageRepository {}
+
+final getIt = GetIt.instance;
+void configure() {
+  getIt.registerFactory<PasswordListRepository>(
+      () => MockPasswordListRepository());
+  getIt.registerFactory<StorageRepository>(() => MockStorageRepository());
+}
+
+void main() {
+  configure();
+  testWidgets('Test HomePage', (tester) async {
+    await tester.pumpWidget(const MainApp());
+    await tester.pumpAndSettle();
+    final finder = find.byElementType(HomePage);
+    expect(finder, findsOneWidget);
+  });
+}
